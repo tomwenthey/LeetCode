f27211b0be87e0e75596c2bbd955908c5b8f354b
tomwenthey <a429273392@qq.com>
2018-07-16 22:48:46 +0800

    200

--- LeetCode2.js
+++ LeetCode2.js
@@ -121,7 +121,6 @@ let a = new ListNode(1);
 let rs = rotateRight(a, 99);
 
 while (rs) {
-  console.log(rs.val);
   rs = rs.next;
 }
 
@@ -149,6 +148,49 @@ var subsets = function(nums) {
   return returnArr;
 };
 
+// 200. Number of Islands
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+  let count = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === "1") {
+        dfs(grid, i, j);
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+let dfs = function(grid, i, j) {
+  if (
+    i < 0 ||
+    j < 0 ||
+    i >= grid.length ||
+    j >= grid[0].length ||
+    grid[i][j] !== "1"
+  ) {
+    return;
+  }
+  grid[i][j] = "#";
+  dfs(grid, i - 1, j);
+  dfs(grid, i + 1, j);
+  dfs(grid, i, j - 1);
+  dfs(grid, i, j + 1);
+};
+
+let landArray = new Array(1);
+landArray[0] = ["1", "1", "0", "1", "0"];
+// landArray[1] = ["1", "1", "0", "0", "0"];
+// landArray[2] = ["0", "0", "1", "0", "0"];
+// landArray[3] = ["0", "0", "0", "1", "1"];
+console.log(numIslands(landArray));
+
 // 202 Happy Number
 
 /**
@@ -163,14 +205,14 @@ var isHappy = function(n) {
     for (let i = 0; i < str.length; i++) {
       sum += parseInt(str[i]) * parseInt(str[i]);
     }
-    console.log(sum);
     if (sum === 1) {
       return true;
     }
+    if (sum === 4) {
+      return false;
+    }
   }
 };
-
-console.log(isHappy(4));
 
 // 563. 二叉树的坡度
 
